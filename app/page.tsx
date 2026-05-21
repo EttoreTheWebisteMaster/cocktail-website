e01fcd13270cf1464fd67da39f73b9a4ded7710d
Ettore Serra <internal.zone.ettore@gmail.com>
2026-05-21 16:46:45 +0200

    Add global.css styles and test on page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <>Hello GSAP!</>
+    <div className="flex-center h-[100vh]">
+      <h1 className="text-3xl text-indigo-300">Hello GSAP!</h1>
+    </div>
   );
 }
