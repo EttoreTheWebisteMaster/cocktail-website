0943efdcbb4bd853355420d8c8ceb3729b08a70c
Ettore Serra <internal.zone.ettore@gmail.com>
2026-05-22 15:43:34 +0200

    Implement the about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Cocktails from "./components/Cocktails";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Cocktails />
+      <About />
     </>
   );
 }
