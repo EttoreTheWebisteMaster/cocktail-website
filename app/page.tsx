5e9092c0cf7c979ac76c3dc7d37391044ecc4278
Ettore Serra <internal.zone.ettore@gmail.com>
2026-05-22 15:19:38 +0200

    Implement cocktails section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Cocktails from "./components/Cocktails";
 import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      
+      <Cocktails />
     </>
   );
 }
